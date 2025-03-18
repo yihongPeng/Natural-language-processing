f7812d44d53faba1fa74fb7d3c360f030c83cf39
TheOne <pyihong143@gmail.com>
2025-03-18 21:08:19 +0800

    update text_preprocessing

--- text_preprocessing.py
+++ text_preprocessing.py
@@ -5,22 +5,23 @@ import re
 
 def read_text(fn = 'wizard_of_os.txt'):
     with open(fn,'r') as f:
-        lines = f.readlines()
-    return [re.sub('[^A-Za-z]+',' ',line).strip().lower() for line in lines]
+        text = f.read()
+    return re.sub('[^A-Za-z1-9]+',' ',text).lower()
 
-def  tokenize(lines,token='word'):
-    if token == 'word':
-        return [line.split() for line in lines]
-    elif token == 'char':
-        return [list(line) for line in lines]
+def  tokenize(text,mode='word'):
+    if mode == 'word':
+        tokens = text.split()
+        return tokens
+    elif mode == 'char':
+        return list(text)
     else:
-        print('Error: Unknown token type' + token)
+        print('Error: Unknown token type' + mode)
 
 # 计算每个词出现的频率(次数)
 
 def count_corpus(tokens):
-    if len(tokens) == 0 or isinstance(tokens[0],list):
-        tokens = [token for line in tokens for token in line]
+    # if len(tokens) == 0 or isinstance(tokens[0],list):
+    #     tokens = [token for line in tokens for token in line]
     return collections.Counter(tokens)
 
 class Vocab:
@@ -44,11 +45,21 @@ class Vocab:
                 self.token2idx[token] = len(self.idx2token) - 1
     def __len__(self):
         return len(self.idx2token)
-    def __getitem__(self,tokens):
-        return [self.token2idx[token] for token in tokens]
+    def __getitem__(self,token):
+        return self.token2idx[token]
+
+def load_corpus_text(max_tokens=-1):
+    text = read_text()
+    tokens = tokenize(text,mode='word')
+    vocab = Vocab(tokens)
+    corpus = [vocab[token] for token in tokens]
+    if max_tokens > 0:
+        corpus = corpus[:max_tokens]
+    return corpus,vocab
 
 if __name__ == '__main__':
-    lines = read_text()
-    tokens = tokenize(lines,'word')
-    vocab = Vocab(tokens)
-    print(vocab[['i', 'am','the', 'one']])
+    # lines = read_text()
+    # tokens = tokenize(lines,'word')
+    # vocab = Vocab(tokens)
+    corpus,vocab = load_corpus_text()
+    print(len(corpus),len(vocab.idx2token))
